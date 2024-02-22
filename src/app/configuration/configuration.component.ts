b5fa663854b38319820a758b04055356eaa6a6e2
Wolfy Fiorini <jonwolffiorini@gmail.com>
2024-02-22 14:49:54 -0800

    added volume sampling in settings

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -9,20 +9,28 @@ import { SettingsService } from '../services/settings.service';
 })
 export class ConfigurationComponent implements OnInit {
 
+  sound: Howl = new Howl({
+    src:['../assets/sample.mp3'],
+    volume: 1,
+    html5: true
+  })
   currentVolume: number = 100
   currentRounds: number = 3
-  currentSongChoices: number = 4
+  currentSongChoices: number = 3
 
   settingsFormGroup: FormGroup = new FormGroup({
-    volume: new FormControl<number>(this.currentVolume, [Validators.min(0), Validators.max(100)]),
-    rounds: new FormControl<number>(this.currentRounds, [Validators.min(1), Validators.max(20)]),
-    songChoices: new FormControl<number>(this.currentSongChoices, [Validators.min(2), Validators.max(10)])
+    volume: new FormControl<number>(100, [Validators.min(0), Validators.max(100)]),
+    rounds: new FormControl<number>(3, [Validators.min(1), Validators.max(20)]),
+    songChoices: new FormControl<number>(3, [Validators.min(2), Validators.max(10)])
   })
 
   constructor(private settingsData: SettingsService) { }
 
   ngOnInit(): void {
-    this.settingsData.currentVolume.subscribe((volumeData) => (this.currentVolume = volumeData))
+    this.settingsData.currentVolume.subscribe((volumeData) => {
+      this.currentVolume = volumeData
+      this.sound.volume(volumeData/100)
+    })
     this.settingsData.currentNumRounds.subscribe((roundsData) => (this.currentRounds = roundsData))
     this.settingsData.currentNumSongChoices.subscribe((choicesData) => (this.currentSongChoices = choicesData))
     this.settingsFormGroup.setValue({
@@ -37,4 +45,14 @@ export class ConfigurationComponent implements OnInit {
     this.settingsData.updateNumRounds(this.settingsFormGroup.controls['rounds'].value)
     this.settingsData.updateNumChoices(this.settingsFormGroup.controls['songChoices'].value)
   }
+
+  updateVolume(): void {
+    this.settingsData.updateVolume(this.settingsFormGroup.controls['volume'].value)
+    this.sound.volume(this.currentVolume / 100)
+  }
+
+  testSound(): void {
+    this.sound.play()
+  }
+
 }
